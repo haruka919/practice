95463fdfd9647ccfa6c3480cd9abcf817b60e9e9
parukoo <hrk.0u0x@gmail.com>
2020-07-23 20:43:42 +0900

    先月の日付を取得

--- calender/main.js
+++ calender/main.js
@@ -3,13 +3,31 @@ console.clear();
 
 {
   const year = 2020;
-  const month = 4; //5月
+  const month = 6; //7月
+
+  function getCalenderHead() {
+    const dates = [];
+    const lastDateOfLastMonth = new Date(year, month, 0).getDate(); // 先月(6/30)の末日を取得 => 30
+    const lastDateOfLastDay = new Date(year, month, 1).getDay(); // 今月(7/1)の曜日を取得 => 3
+
+    for (let i = 0; i < lastDateOfLastDay; i++) {
+      // 30
+      // 29, 30
+      // 28, 29, 30
+      dates.unshift({
+        date: lastDateOfLastMonth - i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+    console.log(dates);
+  }
 
   function getCalenderBody() {
     const dates = []; //date:日付, day:曜日
-    const lastDate = new Date(year, month + 1, 0).getDate(); // 今月の末日を取得
+    const lastDateOfThisMonth = new Date(year, month + 1, 0).getDate(); // 今月の末日を取得
 
-    for (let i = 1; i <=lastDate; i++) {
+    for (let i = 1; i <= lastDateOfThisMonth; i++) {
       dates.push({
         date: i,
         isToday: false,
@@ -20,4 +38,5 @@ console.clear();
   }
 
   getCalenderBody();
+  getCalenderHead();
 }
