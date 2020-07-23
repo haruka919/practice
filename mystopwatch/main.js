c65a06650ee78baba272e2c248d633cc4c3e83b3
parukoo <hrk.0u0x@gmail.com>
2020-07-23 18:29:50 +0900

    カウントアップの実装を追加

--- mystopwatch/main.js
+++ mystopwatch/main.js
@@ -5,4 +5,19 @@
   const start = document.getElementById('start');
   const stop = document.getElementById('stop');
   const reset = document.getElementById('reset');
+
+  let startTime;
+
+  start.addEventListener('click', () => {
+    startTime = Date.now();
+    countUp();
+  });
+
+  function countUp() {
+    console.log(Date.now() - startTime);
+
+    setTimeout(() => {
+      countUp();
+    }, 10);
+  }
 }
