82379af64d9e557013554e1c1c10e2a5354726f1
parukoo <hrk.0u0x@gmail.com>
2020-07-23 19:18:49 +0900

    ボタンの有効無効を切り替え

--- mystopwatch/main.js
+++ mystopwatch/main.js
@@ -10,17 +10,38 @@
   let timeoutId;
   let elapsedTime = 0;
 
+  function setButtonStateInitial() {
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = true;
+  }
+  function setButtonStateRunning() {
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = true;
+  }
+  function setButtonStateStopped() {
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = false;
+  }
+
+  setButtonStateInitial();
+
   start.addEventListener('click', () => {
+    setButtonStateRunning();
     startTime = Date.now();
     countUp();
   });
 
   stop.addEventListener('click', () => {
+    setButtonStateStopped();
     clearTimeout(timeoutId);
     elapsedTime += Date.now() - startTime;
   });
 
   reset.addEventListener('click', () => {
+    setButtonStateInitial();
     timer.textContent = '00:00.000';
     elapsedTime = 0;
   });
