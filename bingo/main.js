cca61eeb0507d0228074221d40b65fb87107e6c2
parukoo <hrk.0u0x@gmail.com>
2020-07-23 09:28:23 +0900

    配列の行と列を反転させる

--- bingo/main.js
+++ bingo/main.js
@@ -4,12 +4,6 @@
   function createColumn(col) {
     const source = [];
     for (let i = 0; i < 15; i++) {
-      // 1...15
-      // source[i] = i + 1 + 15*0;
-      // 16...30
-      // source[i] = i + 1 + 15*1;
-      // 31...45
-      // source[i] = i + 1 + 15*2;
       source[i] = i + 1 + 15 * col;
     }
     const column = [];
@@ -27,5 +21,13 @@
   columns[4] = createColumn(4);
   columns[2][2] = 'FREE';
 
-  console.table(columns);
+  const bingo = [];
+  for (let row = 0;row < 5; row++) {
+    bingo[row] = [];
+    for(let col = 0; col < 5; col++) {
+      bingo[row][col] = columns[col][row];
+    }
+  }
+
+  console.table(bingo);
 }
