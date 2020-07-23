7b2823f76db85f11bb036b0b1a51a1d4ab7bf767
parukoo <hrk.0u0x@gmail.com>
2020-07-23 22:24:59 +0900

    正解とミスをカウントする

--- typinggame/main.js
+++ typinggame/main.js
@@ -3,15 +3,23 @@
   const word = 'apple';  //タイピングする文字
   let loc = 0; //タイピングする位置
 
+  let score = 0;
+  let miss = 0;
+
   const target = document.getElementById('target');
+  const scoreLabel = document.getElementById('score');
+  const missLabel = document.getElementById('miss');
+
   target.textContent = word;
 
   window.addEventListener('keydown', e => {
     if (e.key === word[loc] ) {
-      console.log('score')
+      score++;
       loc++;
+      scoreLabel.textContent = score;
     } else {
-      console.log('miss');
+      miss++;
+      missLabel.textContent = miss;
     }
   });
 }
