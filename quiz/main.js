f44d648faacd91c2c9cbfe027893e301bc6d93ee
parukoo <hrk.0u0x@gmail.com>
2020-07-24 16:21:48 +0900

    選択肢を表示

--- quiz/main.js
+++ quiz/main.js
@@ -11,4 +11,12 @@
   ];
 
   let currentNum = 0;
+
+  question.textContent = quizSet[currentNum].q;
+
+  quizSet[currentNum].c.forEach(choice => {
+    const li = document.createElement('li');
+    li.textContent = choice;
+    choices.appendChild(li);
+  });
 }
