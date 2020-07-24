328afcb41f290815e348c491a870d2920137af73
parukoo <hrk.0u0x@gmail.com>
2020-07-25 06:47:43 +0900

    出題するクイズをシャッフルした

--- quiz/main.js
+++ quiz/main.js
@@ -6,11 +6,11 @@
   const result = document.getElementById('result');
   const scoreLabel = document.querySelector('#result p');
 
-  const quizSet = [
+  const quizSet = shuffle([
     {q: 'What is A?', c:['A0', 'A1', 'A2']},
     {q: 'What is B?', c:['B0', 'B1', 'B2']},
     {q: 'What is C?', c:['C0', 'C1', 'C2']},
-  ];
+  ]);
 
   let currentNum = 0;
   let isAnswered = false;
