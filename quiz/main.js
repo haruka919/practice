85b8baf17d9a57175189e58f2ffdfb0b405ac06f
parukoo <hrk.0u0x@gmail.com>
2020-07-24 16:37:41 +0900

    選択肢をシャッフルした

--- quiz/main.js
+++ quiz/main.js
@@ -25,7 +25,8 @@
     return arr;
   }
 
-  quizSet[currentNum].c.forEach(choice => {
+  const shuffledChoices = shuffle([...quizSet[currentNum].c]); //スプレッド演算子にすることで参照されない。コピーの配列を新たに作っている
+  shuffledChoices.forEach(choice => {
     const li = document.createElement('li');
     li.textContent = choice;
     choices.appendChild(li);
