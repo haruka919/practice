77c591f84c3c2183e0de1d5308d9a4a7bf3e3acc
parukoo <hrk.0u0x@gmail.com>
2020-07-25 08:08:37 +0900

    spinメソッドを定義

--- slot/main.js
+++ slot/main.js
@@ -18,6 +18,19 @@
       const main = document.querySelector('main');
       main.appendChild(section);
     }
+
+    getRandomImage() {
+      const images = [
+        'img/seven.png',
+        'img/bell.png',
+        'img/cherry.png',
+      ];
+      return images[Math.floor(Math.random() * images.length)];
+    }
+
+    spin() {
+      this.img.src = this.getRandomImage();
+    }
   }
 
   const panels = [
@@ -25,4 +38,11 @@
     new Panel(),
     new Panel(),
   ];
+
+  const spin = document.getElementById('spin');
+  spin.addEventListener('click', () => {
+    panels.forEach(panel => {
+      panel.spin();
+    });
+  });
 }
